13d5b78cfe922ce3d8cdd643e89f427d4d596b59
Anton Sementsov <anton.sementsov@agenatech.com>
2021-11-28 20:20:29 +0200

    moved old client

---
+++ client/src/features/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Contact.css"
+
+export const Contact =()=>{
+    return (
+        <div>
+            <span>@shamievdavid</span>
+            <br/>
+            <span>@kunstkun</span>
+            
+        </div>
+    )
+}
