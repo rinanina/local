259e654c4c6525c18ef19b1355eb9e19cb07a94c
Shamiev David <shamievdave@gmail.com>
2021-12-22 17:30:23 +0200

    add header fix & language context

--- client/src/features/Header/Header.jsx
+++ client/src/features/Header/Header.jsx
@@ -6,6 +6,8 @@ import { Navigation } from 'features/Navigation';
 import { Wrapper, Title } from './styled';
 import Language from 'features/Language/Language';
 
+
+
 const Header = () => (
   <Wrapper>
     <Title>
