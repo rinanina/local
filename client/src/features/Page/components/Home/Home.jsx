a1831710755fada626a116e8224d77be9e73c009
khal <kateryna.k@sbtech.com>
2022-01-06 18:02:42 +0200

    add logout

--- client/src/features/Page/components/Home/Home.jsx
+++ client/src/features/Page/components/Home/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import { useAuth } from 'context/AuthContext';
+
 import PageWrapper from '../PageWrapper';
 
-const Home = () => <PageWrapper>Home</PageWrapper>;
+const Home = () => {
+  const { isAuth } = useAuth();
+
+  return (
+    <PageWrapper>
+      <span>Home</span>
+      {isAuth && <span>Admin here</span>}
+    </PageWrapper>
+  );
+};
 
 export default Home;
