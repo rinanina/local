b947c3cc7e95599dcf08827556d9baae45a16194
Anton Sementsov <anton.sementsov@agenatech.com>
2022-01-11 00:31:19 +0200

    fixes

--- client/src/features/Archive/components/StickerbookForm/StickerbookForm.jsx
+++ client/src/features/Archive/components/StickerbookForm/StickerbookForm.jsx
@@ -54,25 +54,34 @@ const StickerbookForm = ({ data }) => {
     }
   }, [data]);
 
-  // TODO: fix so data will be send only on submit and not slide add
   const onSubmitHandler = async (data) => {
     console.log('onSubmitHandler');
     console.log('data', data);
-
     await doFetch((api) => api.stickerbooks.create(data));
+  };
+
+  const onAddSlideHandler = (event) => {
+    event.preventDefault();
+    append(slideData);
   };
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmitHandler)}>
         <FormElement>
-          <Button text='Add slide' onClick={() => append(slideData)} />
+          <Button text="Add slide" onClick={onAddSlideHandler} />
         </FormElement>
         <FormElement>
-          <Button type='submit' text='Publish' />
+          <Button type="submit" text="Publish" />
         </FormElement>
         <FormElement>
-          <input type='text' defaultValue={values?.title} name='title' placeholder='Title' {...register('title')} />
+          <input
+            type="text"
+            defaultValue={values?.title}
+            name="title"
+            placeholder="Title"
+            {...register('title')}
+          />
         </FormElement>
         <div>
           {fields.map((item, index) => (
@@ -88,7 +97,7 @@ const StickerbookForm = ({ data }) => {
                       <FormElement>
                         <textarea
                           name={`description-${index}`}
-                          placeholder='Slide Description'
+                          placeholder="Slide Description"
                           defaultValue={data?.slides[index]?.description}
                           // error={slideErrors && slideErrors['description']?.message}
                           {...register(`slides[${index}].description`)}
@@ -96,36 +105,38 @@ const StickerbookForm = ({ data }) => {
                       </FormElement>
                       <FormElement>
                         <input
-                          type='text'
+                          type="text"
                           name={`artistName-${index}`}
-                          placeholder='Artist Name'
-                          defaultValue={data.slides[index].artistName}
+                          placeholder="Artist Name"
+                          defaultValue={data?.slides[index].artistName}
                           // error={slideErrors && slideErrors['artistName']?.message}
                           {...register(`slides[${index}].artistName`)}
                         />
                       </FormElement>
                       <FormElement>
                         <input
-                          type='text'
+                          type="text"
                           name={`linkToPage-${index}`}
-                          placeholder='Link to artist page'
-                          defaultValue={data.slides[index].linkToPage}
+                          placeholder="Link to artist page"
+                          defaultValue={data?.slides[index].linkToPage}
                           // error={slideErrors && slideErrors['linkToPage']?.message}
                           {...register(`slides[${index}].linkToPage`)}
                         />
                       </FormElement>
                       <FormElement>
                         <input
-                          type='file'
+                          type="file"
                           name={`image-${index}`}
-                          placeholder='Load image'
+                          placeholder="Load image"
                           // error={slideErrors && slideErrors['image']?.message}
                           {...register(`slides[${index}].image`)}
                         />
-                        {data?.slides[index] && <Image src={data.slides[index].image} />}
+                        {data?.slides[index] && (
+                          <Image src={data?.slides[index].image} />
+                        )}
                       </FormElement>
                       <FormElement>
-                        <Button text='Delete' onClick={() => remove(index)} />
+                        <Button text="Delete" onClick={() => remove(index)} />
                       </FormElement>
                     </div>
                   );
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -7,20 +7,33 @@ const builder = (httpClient) => ({
     login: (data) => httpClient.post(Endpoint.LOGIN, data),
   },
   stickerbooks: {
-    create: (data) => httpClient.post(Endpoint.CREATE_STICKERBOOK, data),
+    create: (data) => {
+      const config = {
+        headers: { 'content-type': 'multipart/form-data' },
+      };
+      
+
+      return httpClient.post(
+        Endpoint.CREATE_STICKERBOOK,
+        data,
+        // TODO: Convert request data from JSON to FormData
+        // jsonToFormData(data),
+        config
+      );
+    },
     loadAll: () => httpClient.get(Endpoint.LOAD_STICKERBOOKS),
-    loadOne: (id) => httpClient.get(`${Endpoint.LOAD_STICKERBOOKS}/${id}`)
+    loadOne: (id) => httpClient.get(`${Endpoint.LOAD_STICKERBOOKS}/${id}`),
   },
   artists: {
     create: (data) => httpClient.post(Endpoint.CREATE_ARTIST, data),
     loadAll: () => httpClient.get(Endpoint.LOAD_ARTISTS),
-    loadOne: (id) => httpClient.get(`${Endpoint.LOAD_ARTISTS}/${id}`)
+    loadOne: (id) => httpClient.get(`${Endpoint.LOAD_ARTISTS}/${id}`),
   },
   posts: {
     create: (data) => httpClient.post(Endpoint.CREATE_POST, data),
     loadAll: () => httpClient.get(Endpoint.LOAD_POSTS),
-    loadOne: (id) => httpClient.get(`${Endpoint.LOAD_POSTS}/${id}`)
-  }
+    loadOne: (id) => httpClient.get(`${Endpoint.LOAD_POSTS}/${id}`),
+  },
 });
 
 export default builder(httpClient);
